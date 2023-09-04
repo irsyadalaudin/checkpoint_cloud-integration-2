5ff263e75c12093046caef27bd2f28a2977ccb94
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2023-09-04 14:11:41 +0300

    added Desc.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css'
+import Desc from './components/Desc'
 import Navbar from './components/Navbar'
 
 const App = () => {
 	return (
-		<Navbar />
+		<>
+			<Navbar />
+			<Desc />
+		</>
 	)
 }
 
---
+++ src/components/Desc.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/no-unescaped-entities */
+const Desc = () => {
+    return (
+        <div className='pt-10 px-20 mb-14 flex'>
+            <div className='flex-1'>
+                <h1>Get in touch with us</h1>
+                <p>We love chocolate chip cookies as much as you do! Our team is always experimenting with new recipes to create the perfect chocolate chip cookie. Whether you like them soft and chewy or crispy and crunchy, we've got a cookie for you. Come visit us and try one (or more!) of our delicious chocolate chip cookies.</p>
+            </div>
+            <img src='https://i.postimg.cc/m2H8k2XF/cookies.png' alt='cookies-main' />
+        </div>
+    )
+}
+
+export default Desc
