d8e3233b76a1d20d358facc3bdb4d6eb8a8dc516
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2023-09-04 15:31:37 +0300

    added comment in Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,15 @@
 const Navbar = () => {
     return (
         <>
+            {/* Create a navigation bar with background color, height, and sticky positioning at the top */}
             <nav className='bg-amber-900 h-14 flex justify-between items-center px-20 sticky top-0'>
+
+                {/* Display a logo image with a specific height */}
                 <img className='h-12' src='https://i.postimg.cc/W4YSCwg3/cookies-logo.png' alt='cookies-logo' />
+
+                {/* Create a navigation menu using a div element */}
                 <div>
+                    {/* Create navigation links with padding and href attributes */}
                     <a className='pl-5' href='#'>Home</a>
                     <a className='pl-5' href='#'>Biskuit</a>
                     <a className='pl-5' href='#'>About</a>
@@ -14,4 +20,5 @@ const Navbar = () => {
     )
 }
 
+// Export the Navbar component as the default export
 export default Navbar
